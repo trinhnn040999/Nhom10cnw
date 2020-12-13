f5109cb6980cbbe2fbc13bdeb9706c95d4aa8825
phucnq <nguyenquyphuc1591999@gmail.com>
2020-12-13 21:56:50 +0700

    OK

--- btl/routes/register.js
+++ btl/routes/register.js
@@ -30,42 +30,49 @@ app.post('/', function(req, res) {
     var fullname = req.body.username
     var email = req.body.email
 
-    // kiem tra email da ton tai chua
-    connection.query("SELECT accounts.email from accounts where email = ?", email, (error, results, fields) => {
-        if (error) {
-            throw error
-        } else {
-            if (results.length > 0) {
-                // neu da ton tai email
-                res.render('login', { thongBao: 'Error register, email already exists!', color: 'red' })
-            } else {
-                // neu chua ton tai
-                // kiem tra password confirm co bang password hay khong
-                if (confirm === password) {
-                    var user = {
-                            'username': username,
-                            'password': password,
-                            'email': email,
-                            'fullname': fullname,
-                        }
-                        // them thong tin vao co so du lieu
-                    connection.query('INSERT INTO accounts SET ?', user, function(error, results, fields) {
-                        // neu co loi xay ra
-                        if (error) {
-                            // thong bao loi
-                            res.render('login', { thongBao: 'Error register', color: 'red' })
-                        } else {
-                            // thong bao thanh cong
-                            res.render('login', { thongBao: 'Success register, please login.', color: 'green' })
-                        }
-                    })
+    connection.query("select * from accounts where username=?", username, (error, results, fields) => {
 
+        if (results.length != 0) {
+            res.render('login', { thongBao: 'Error register, username already exists!', color: 'red' })
+        } else {
+            connection.query("SELECT accounts.email from accounts where email = ?", email, (error, results, fields) => {
+                if (error) {
+                    throw error
                 } else {
-                    res.render('login', { thongBao: 'Error register, password confirm incorrect!', color: 'red' })
+                    if (results.length > 0) {
+                        // neu da ton tai email
+                        res.render('login', { thongBao: 'Error register, email already exists!', color: 'red' })
+                    } else {
+                        // neu chua ton tai
+                        // kiem tra password confirm co bang password hay khong
+                        if (confirm === password) {
+                            var user = {
+                                    'username': username,
+                                    'password': password,
+                                    'email': email,
+                                    'fullname': fullname,
+                                }
+                                // them thong tin vao co so du lieu
+                            connection.query('INSERT INTO accounts SET ?', user, function(error, results, fields) {
+                                // neu co loi xay ra
+                                if (error) {
+                                    // thong bao loi
+                                    res.render('login', { thongBao: 'Error register', color: 'red' })
+                                } else {
+                                    // thong bao thanh cong
+                                    res.render('login', { thongBao: 'Success register, please login.', color: 'green' })
+                                }
+                            })
+                        } else {
+                            res.render('login', { thongBao: 'Error register, password confirm incorrect!', color: 'red' })
+                        }
+                    }
                 }
-            }
+            })
         }
     })
+
+    // kiem tra email da ton tai chua
 
 
 });
