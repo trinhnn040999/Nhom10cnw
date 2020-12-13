ae7f750f245f1cfa185c3532e783938b4daf42e5
phucnq <nguyenquyphuc1591999@gmail.com>
2020-12-13 20:59:34 +0700

    OK

--- btl/routes/broad.js
+++ btl/routes/broad.js
@@ -54,17 +54,17 @@ app.post('/createTodo', function(req, res, next) {
 })
 
 
-app.post('/invite', function(req, res, next){
+app.post('/invite', function(req, res, next) {
     var newmember = {
-        'id' : req.cookies.id_broad,
-        'email' : req.body.email,
+        'id': req.cookies.id_broad,
+        'email': req.body.email,
         'broadName': req.cookies.broadName
     }
     console.log(newmember['id']);
     console.log(newmember['email']);
     console.log(newmember['broadName']);
-    connection.query('insert into broad set ?', newmember, (err, results, fields)=>{
-        if(err) throw err
+    connection.query('insert into broad set ?', newmember, (err, results, fields) => {
+        if (err) throw err
         console.log('Invite Sucess!!!');
     })
 })
@@ -130,11 +130,4 @@ app.post('/update_favourite', function(req, res, next) {
     })
 })
 
-
-<<<<<<< HEAD
-
-
 module.exports = app;
-=======
-module.exports = app;
->>>>>>> e4a302e46bf177a133017d2dd81aa0b01d2977c2
