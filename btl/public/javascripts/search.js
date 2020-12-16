5b7ef65de7d028a1eef2a002ea748cca7cdf9cbf
peartalent <peartalent@gmail.com>
2020-12-16 14:28:28 +0700

    ud

--- btl/public/javascripts/search.js
+++ btl/public/javascripts/search.js
@@ -2,6 +2,7 @@
 // Khi người dùng nhập key vào, nó sẽ lấy dữ liệu từ db đổ vào , cái này chỉ search broad thôi.
 $(document).ready(function(){
     $("input.form-control.form-control-sm.ml-3.w-75.dropdown-toggle").keydown(function(){
+    
         $('#resultSearch').empty(); //Hàm xóa hết các li bên trong
         // Hàm append này chỉ cần thay nd CNWeb với link thẻ a, link img
         $('#resultSearch').append(
